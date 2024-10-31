9cf59881306cd91daf60a46a4dc4edd6bef6661a
SehaKim <aiostpch@naver.com>
2024-10-31 22:02:58 +0900

    map

---
+++ src/main/java/com/example/project/map/MapController.java
@@ -0,0 +1,13 @@
+package com.example.project.map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("map")
+    public String mapPage(){
+        return "map";
+    }
+}
